1e77275df6d5c5fc3ab5fc971cfdb017f52d36ff
Pranav Vyas <pranavvyas2016@gmail.com>
2022-03-15 12:25:51 +0530

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+        <Route exact path="/register"  element={<Register/>}></Route>
         <Route exact path='/todo' element={< Todo />}></Route>
         <Route exact path='/login' element={<Login/>}></Route>
-        <Route exact path='/register' element={<Register/>}></Route>
         </Routes>
       </Router>
     </div>
